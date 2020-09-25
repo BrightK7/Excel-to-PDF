4da08a769f5e50ce3af3f5f449ad907fb65dca20
Jin Li <jinli@Jins-iMac.local>
2020-09-24 21:09:59 -0600

    version 2

--- api_test/quickstart.py
+++ api_test/quickstart.py
@@ -4,6 +4,6 @@ from pydrive.drive import GoogleDrive
 gauth = GoogleAuth()
 gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication.
 drive = GoogleDrive(gauth)
-file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+file_list = drive.ListFile({'q'=sharedWithMe and name contains 'Meaghan'}).GetList()
 for file1 in file_list:
   print('title: %s, id: %s' % (file1['title'], file1['id']))
